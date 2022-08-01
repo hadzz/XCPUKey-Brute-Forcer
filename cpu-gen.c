75434d70fcca1b6937b5fd56d8e41af68aea6c4f
hadzz <ajh9515@gmail.com>
2022-08-01 18:22:25 -0400

    Fixed memory leak in RC4 decryption
    - Windows (mingw) now doesn't crash
    - Other cleanup
    - mutex locks added for cleaner printing

--- cpu-gen.c
+++ cpu-gen.c
@@ -36,14 +36,16 @@
 
 // Globals.
 unsigned char * cpu_key, * serial_num;
-uint8_t * kvheader;
+uint8_t * kv_enc;
 uint8_t * hmac;
 
 int debug = 0, mine = 1;
 int nthreads = 0;
 uint64_t total_keys = 0;
-uint64_t valid_keys = 0;
+unsigned int valid_keys = 0;
 bool success = false;
+
+pthread_mutex_t print_mutex;
 
 typedef struct ModeOf {
   uint64_t Start;
@@ -55,8 +57,8 @@ typedef struct ModeOf {
 int verifyCPUKey(unsigned char * key) {
   int i, j;
   int hamming = 0;
-  for (i = 0; i < 13; i++) {
-    for (j = 0; j < 8; j++) {
+  for (i = 0; i < 13; ++i) {
+    for (j = 0; j < 8; ++j) {
       unsigned char mask = 1 << j;
       if ((key[i] & mask) != 0)
         hamming++;
@@ -70,11 +72,11 @@ int verifyCPUKey(unsigned char * key) {
 
 void keyECD(unsigned char * key, unsigned char * ecd) {
   int cnt;
-  for (cnt = 0; cnt < 16; cnt++)
+  for (cnt = 0; cnt < 16; ++cnt)
     ecd[cnt] = key[cnt];
 
   unsigned int acc1 = 0, acc2 = 0;
-  for (cnt = 0; cnt < 0x80; cnt++, acc1 >>= 1) {
+  for (cnt = 0; cnt < 0x80; ++cnt, acc1 >>= 1) {
     unsigned char bTmp = ecd[cnt >> 3];
     unsigned int dwTmp = (unsigned int)((bTmp >> (cnt & 7)) & 1);
     if (cnt < 0x6A) {
@@ -95,7 +97,6 @@ int previous_power_of_two( int x ) {
     if (x == 0) {
         return 0;
     }
-    // x--; Uncomment this, if you want a strictly less than 'x' result.
     x |= (x >> 1);
     x |= (x >> 2);
     x |= (x >> 4);
@@ -104,26 +105,11 @@ int previous_power_of_two( int x ) {
     return x - (x >> 1);
 }
 
-
 /*
  * Brute Thread. old fashioned brute force the cpukey.
  */
 void * brute_thread(void * buf) {
   my_mode_t * tm = buf;
-  printf("#%d Thread Started (Starting Index: %"
-    PRIu64 ", Max Index: %"
-    PRIu64 ", Mode: Increment)...\n",
-    tm -> ThreadNum, tm -> Start, tm -> Max);
-  // testing key: DF AC 65 E8 B2 52 4E B8 4A 6B 7D 20 70 35 06 0C
-  // serial: 057469473207
-  // corona: 0x5F, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x14, 0x1E, 0xFB
-  // corona: 150273303105
-  //unsigned char key[16] = { 0xDF, 0xAC, 0x65, 0xE8, 0xB2,
-  //0x52, 0x4E, 0xB8, 0x4A, 0x6B, 0x7D, 0x20, 0x70, 0x35,
-  //0x06, 0x0C };
-  //unsigned char key[16] = { 0x5F, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-  //0x00, 0x00, 0x00, 0x00, 0x00, 0x14, 0x1E, 0xFB };
-  // full cpu key used to decrypt kv
   unsigned char key[16];
   unsigned char * kp = key;
   // right part of key itterated normally (22 bits actually used)
@@ -137,19 +123,19 @@ void * brute_thread(void * buf) {
   unsigned char * hmac_key = malloc(16);
 
   // Init crypto support.
-  rc4_state_t * rc4 = malloc(sizeof(rc4_state_t));
+  struct rc4_state * rc4 = malloc(sizeof(struct rc4_state));
   // Outputs.
-  uint8_t buf2[20];
-  uint8_t * buf4 = malloc(172);
+  uint8_t hmac_key_uint[20];
+  uint8_t * kv_dec = malloc(172);
 
   int kn = 0, i, j;
-  uint64_t thread_valid_keys = 0;
+  int print_ctr = 0;
   
   // Copy hmac.
   kn = 0;
   while (kn < 16) {
     hmac_buf[kn] = (unsigned char) hmac[kn];
-    kn++;
+    ++kn;
   }
 
   // Init keyLeft
@@ -157,30 +143,42 @@ void * brute_thread(void * buf) {
   int prefix_bits = log2(nthreads);  // 8 threads = 3 prefix bits
   int bitsRemaining=53;
   j=prefix_bits-1;
-  for (i = 0; i < prefix_bits; i++) {
+  for (i = 0; i < prefix_bits; ++i) {
     if (((tm->ThreadNum >> j) & 1)) {
       keyLeft[i] = 1;
       bitsRemaining--;
     }
+    else keyLeft[i] = 0;
     j--;
   }
-  for (i = prefix_bits; i < prefix_bits + bitsRemaining; i++)
+  for (i = prefix_bits; i < prefix_bits + bitsRemaining; ++i)
     keyLeft[i] = 1;
-  for (i = prefix_bits + bitsRemaining; i < 106; i++)
+  for (i = prefix_bits + bitsRemaining; i < 106; ++i)
     keyLeft[i] = 0;
   
+  // set key's upper 13 bytes to 0
+  for (i = 0; i < 13; ++i)
+    kp[i] = 0;
+  // copy / convert bit array into upper 13 bytes of key
+  for (i = 0; i < 104; ++i) {
+    kp[i / 8] |= ((keyLeft[i]) << (7 - (i % 8)));
+  }
+  
+  pthread_mutex_lock(&print_mutex);
+  printf("#%d Thread Started (Prefix: %02X"
+    ", Mode: Gospers Hack)...\n",
+    tm -> ThreadNum, tm -> ThreadNum);
+  printf("Initial Key: ");
+  kn = 0;
+  while (kn < 16) {
+    printf("%02X", kp[kn]);
+    ++kn;
+  }
+  printf("\n");
+  pthread_mutex_unlock(&print_mutex);
 
   bool done = false;
   while (!done && mine > 0) {
-    
-    // set key's upper 13 bytes to 0
-    for (i = 0; i < 13; i++)
-      kp[i] = 0;
-    // copy / convert bit array into upper 13 bytes of key
-    for (i = 0; i < 104; i++) {
-      kp[i / 8] |= ((keyLeft[i]) << (7 - (i % 8)));
-    }
-    
     // upper 6 bits of 14th byte come from keyRight
     kp[13] = (unsigned char)(((keyRight & 0xFF0000) >> 14));
     // 14th byte has 2 bits from keyLeft, mask 0x03
@@ -188,6 +186,7 @@ void * brute_thread(void * buf) {
     if (keyLeft[105] == 1) kp[13] += 2;
     kp[14] = (unsigned char)((keyRight & 0xFF00) >> 8);
     kp[15] = (unsigned char)(keyRight & 0xFF);
+    
     while (mine > 0) {
       
       // check ECD to see if this key is valid
@@ -197,7 +196,7 @@ void * brute_thread(void * buf) {
           keyRight = 0;
           break;
         }
-        keyRight++;
+        ++keyRight;
         kp[13] = (unsigned char)(((keyRight & 0xFF0000) >> 14) | (kp[13] & 0x3));
         kp[14] = (unsigned char)((keyRight & 0xFF00) >> 8);
         kp[15] = (unsigned char)(keyRight & 0xFF);
@@ -205,49 +204,55 @@ void * brute_thread(void * buf) {
         continue;
       }
       
-      
-      if (thread_valid_keys % 20 == 0) {
+      ++print_ctr; 
+      ++total_keys;
+      ++valid_keys;
+      if (print_ctr == 25) {
+        pthread_mutex_lock(&print_mutex);
         printf("Thread %d Current Key: ", tm -> ThreadNum);
         kn = 0;
         while (kn < 16) {
           printf("%02X", kp[kn]);
-          kn++;
+          ++kn;
         }
         printf("\n");
+        print_ctr = 0;
+        pthread_mutex_unlock(&print_mutex);
       }
       
-      thread_valid_keys++;
       // Generate Hmac Key for decryption.
       hmac_sha1(hmac_key, kp, 128, hmac_buf, 128);
 
       // Convert Hmac to uint8_t.
       kn = 0;
       while (kn < 16) {
-        buf2[kn] = (uint8_t) hmac_key[kn];
-        kn++;
+        hmac_key_uint[kn] = (uint8_t) hmac_key[kn];
+        ++kn;
       }
+      
       // Decrypt kv_header and compare to known serial number.
-      rc4_init(rc4, buf2, 16);
-      rc4_crypt(rc4, kvheader, &buf4, 172);
+      rc4_init(rc4, hmac_key_uint, 16);
+      rc4_crypt(rc4, kv_enc, kv_dec, 172);
+      //rc4_v(kv_enc, kv_dec, 172, hmac_key_uint, 16);
+      
       // Compare The serial number.
-      if (buf4[171] == (uint8_t) serial_num[11] &&
-        buf4[170] == (uint8_t) serial_num[10] &&
-        buf4[169] == (uint8_t) serial_num[9] &&
-        buf4[168] == (uint8_t) serial_num[8] &&
-        buf4[167] == (uint8_t) serial_num[7] &&
-        buf4[166] == (uint8_t) serial_num[6] &&
-        buf4[165] == (uint8_t) serial_num[5] &&
-        buf4[164] == (uint8_t) serial_num[4] &&
-        buf4[163] == (uint8_t) serial_num[3] &&
-        buf4[162] == (uint8_t) serial_num[2] &&
-        buf4[161] == (uint8_t) serial_num[1] &&
-        buf4[160] == (uint8_t) serial_num[0]) {
+      if (kv_dec[171] == (uint8_t) serial_num[11] &&
+        kv_dec[170] == (uint8_t) serial_num[10] &&
+        kv_dec[169] == (uint8_t) serial_num[9] &&
+        kv_dec[168] == (uint8_t) serial_num[8] &&
+        kv_dec[167] == (uint8_t) serial_num[7] &&
+        kv_dec[166] == (uint8_t) serial_num[6] &&
+        kv_dec[165] == (uint8_t) serial_num[5] &&
+        kv_dec[164] == (uint8_t) serial_num[4] &&
+        kv_dec[163] == (uint8_t) serial_num[3] &&
+        kv_dec[162] == (uint8_t) serial_num[2] &&
+        kv_dec[161] == (uint8_t) serial_num[1] &&
+        kv_dec[160] == (uint8_t) serial_num[0]) {
         kn = 0;
         while (kn < 16) {
           cpu_key[kn] = kp[kn];
-          kn++;
+          ++kn;
         }
-        printf("\n");
         mine = 0;
         done = true;
         success = true;
@@ -259,39 +264,31 @@ void * brute_thread(void * buf) {
           // go to next keyLeft code
           break;
         }
-        keyRight++;
+        ++keyRight;
         kp[13] = (unsigned char)(((keyRight & 0xFF0000) >> 14) | (kp[13] & 0x3));
         kp[14] = (unsigned char)((keyRight & 0xFF00) >> 8);
         kp[15] = (unsigned char)(keyRight & 0xFF);
-        valid_keys++;
         
       }
     }
     if (done) break;
     // get the next valid bit permutation for keyLeft with weight 53
-    for (i = prefix_bits; i < 105; i++) {
+    for (i = prefix_bits; i < 105; ++i) {
       // from right to left: look for first one with a zero on its left
       if (keyLeft[i] == 1 && keyLeft[i + 1] == 0) {
-        /*if (i == tm->Max)
-        {
-          printf("Thread %d exiting...\n", tm->ThreadNum);
-          done = true;
-          break;
-        }
-        */
         // swap
         keyLeft[i] = 0;
         keyLeft[i + 1] = 1;
         // shift all one bits from 0 to i to the far left
         int oneBits = 0;
         int zeroBits = 0;
-        for (j = prefix_bits; j < i; j++) {
+        for (j = prefix_bits; j < i; ++j) {
           if (keyLeft[j] == 1) oneBits++;
           else zeroBits++;
         }
-        for (j = prefix_bits; j < prefix_bits + oneBits; j++)
+        for (j = prefix_bits; j < prefix_bits + oneBits; ++j)
           keyLeft[j] = 1;
-        for (j = prefix_bits + oneBits; j < i; j++)
+        for (j = prefix_bits + oneBits; j < i; ++j)
           keyLeft[j] = 0;
         
         break;
@@ -302,13 +299,20 @@ void * brute_thread(void * buf) {
         printf("Thread %d exiting...\n", tm->ThreadNum);
       }
     }
+    // set key's upper 13 bytes to 0
+    for (i = 0; i < 13; ++i)
+      kp[i] = 0;
+    // copy / convert bit array into upper 13 bytes of key
+    for (i = 0; i < 104; ++i) {
+      kp[i / 8] |= ((keyLeft[i]) << (7 - (i % 8)));
+    }
   }
   free(keyLeft);
   free(ecd);
   free(hmac_buf);
   free(hmac_key);
   free(rc4);
-  free(buf4);
+  free(kv_dec);
   pthread_exit(NULL);
   return NULL;
 }
@@ -374,8 +378,6 @@ int main(int argc, char ** argv) {
     printf("Unable to open kv file...\n");
     return 1;
   }
-  printf(argv[1]);
-  printf("\n");
   fseek(kv, 0, SEEK_SET);
   hmac = malloc(16);
   if (fread(hmac, 16, 1, kv) != 1) {
@@ -388,13 +390,13 @@ int main(int argc, char ** argv) {
     t = 0;
     while (t < 16) {
       printf("%02X", hmac[t]);
-      t++;
+      ++t;
     }
     printf("\n");
   }
-  kvheader = malloc(16368);
+  kv_enc = malloc(16368);
   fseek(kv, 16, SEEK_SET);
-  if (fread(kvheader, 16368, 1, kv) != 1) {
+  if (fread(kv_enc, 16368, 1, kv) != 1) {
     fprintf(stderr, "Unable to read kv header from KeyVault...\n");
     return 1;
   }
@@ -402,8 +404,8 @@ int main(int argc, char ** argv) {
     printf("KV Header : ");
     t = 0;
     while (t < 16368) {
-      printf("%02X", kvheader[t]);
-      t++;
+      printf("%02X", kv_enc[t]);
+      ++t;
     }
     printf("\n");
   }
@@ -419,7 +421,7 @@ int main(int argc, char ** argv) {
       if (debug == 1) {
         printf("%02X", serial_num[t]);
       }
-      t++;
+      ++t;
     }
     if (debug == 1) {
       printf("\n");
@@ -456,9 +458,10 @@ int main(int argc, char ** argv) {
   }
 
   pthread_t threads[nthreads];
+  pthread_mutex_init(&print_mutex, NULL);
 
   // Start Mining Threads.
-  for (t = 0; t < nthreads; t++) {
+  for (t = 0; t < nthreads; ++t) {
     my_mode_t * buf = malloc(sizeof(*buf));
     buf -> ThreadNum = t;
     buf -> Start = ((53 / nthreads) * (t+1));
@@ -483,25 +486,27 @@ int main(int argc, char ** argv) {
     hashes = total_keys - start;
     if ((tv_now.tv_sec - last_upd.tv_sec) >= 5) {
       printf("[ KPS : %d k/ps - Total : %"
-        PRIu64 " - Valid Keys : %"
-        PRIu64 " ]\n", hashes, total_keys, valid_keys);
+        PRIu64 " - Valid Keys : %d ]\n",
+        hashes, total_keys, valid_keys);
       gettimeofday( & last_upd, NULL);
     }
 
     gettimeofday( & tv_now, NULL);
   }
-  for (t = 0; t < nthreads; t++) {
+  for (t = 0; t < nthreads; ++t) {
     pthread_join(threads[t], NULL);
   }
+  // destroy mutex
+  pthread_mutex_destroy(&print_mutex);
   
   if (success)
   {
-    printf("KEY FOUND!!!!!!!! :-)\n");
+    printf("\nKEY WAS FOUND! :)\n");
     printf("CPU Key : ");
     t = 0;
     while (t < 16) {
       printf("%02X", cpu_key[t]);
-      t++;
+      ++t;
     }
     printf("\n");
   }
@@ -510,7 +515,7 @@ int main(int argc, char ** argv) {
   }
   
   free(hmac);
-  free(kvheader);
+  free(kv_enc);
   free(serial_num);
   free(cpu_key);
   return (EXIT_SUCCESS);
--- crypto/enc/rc4.c
+++ crypto/enc/rc4.c
@@ -1,54 +1,101 @@
+/*
+ * rc4.c
+ *
+ * Copyright (c) 1996-2000 Whistle Communications, Inc.
+ * All rights reserved.
+ *
+ * Subject to the following obligations and disclaimer of warranty, use and
+ * redistribution of this software, in source or object code forms, with or
+ * without modifications are expressly permitted by Whistle Communications;
+ * provided, however, that:
+ * 1. Any and all reproductions of the source or object code must include the
+ *    copyright notice above and the following disclaimer of warranties; and
+ * 2. No rights are granted, in any manner or form, to use Whistle
+ *    Communications, Inc. trademarks, including the mark "WHISTLE
+ *    COMMUNICATIONS" on advertising, endorsements, or otherwise except as
+ *    such appears in the above copyright notice or in the software.
+ *
+ * THIS SOFTWARE IS BEING PROVIDED BY WHISTLE COMMUNICATIONS "AS IS", AND
+ * TO THE MAXIMUM EXTENT PERMITTED BY LAW, WHISTLE COMMUNICATIONS MAKES NO
+ * REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED, REGARDING THIS SOFTWARE,
+ * INCLUDING WITHOUT LIMITATION, ANY AND ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
+ * WHISTLE COMMUNICATIONS DOES NOT WARRANT, GUARANTEE, OR MAKE ANY
+ * REPRESENTATIONS REGARDING THE USE OF, OR THE RESULTS OF THE USE OF THIS
+ * SOFTWARE IN TERMS OF ITS CORRECTNESS, ACCURACY, RELIABILITY OR OTHERWISE.
+ * IN NO EVENT SHALL WHISTLE COMMUNICATIONS BE LIABLE FOR ANY DAMAGES
+ * RESULTING FROM OR ARISING OUT OF ANY USE OF THIS SOFTWARE, INCLUDING
+ * WITHOUT LIMITATION, ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * PUNITIVE, OR CONSEQUENTIAL DAMAGES, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES, LOSS OF USE, DATA OR PROFITS, HOWEVER CAUSED AND UNDER ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF WHISTLE COMMUNICATIONS IS ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ * $FreeBSD: src/sys/crypto/rc4/rc4.c,v 1.2.2.1 2000/04/18 04:48:31 archie Exp $
+ */
+#include <stdint.h>
 #include "rc4.h"
 
-/*
- * RC4 Init State.
- */
-void rc4_init(rc4_state_t* state, const uint8_t *key, int keylen)
+static inline void
+swap_bytes(u_char *a, u_char *b)
 {
-    state->i = 0;
-    state->j = 0;
-    if(state->x == 0) {
-        state->s_box_len = 0x100;
-        state->sbox = malloc(0x100);
-        state->x = 1;
-    }
-    // Init sbox.
-    int i = 0, index = 0, j = 0;
-    uint8_t buf;
-    while(i < state->s_box_len) {
-        state->sbox[i] = (uint8_t)i;
-        i++;
-    }
-    while(j < state->s_box_len) {
-        index = ((index + state->sbox[j]) + key[j % keylen]) % state->s_box_len;
-        buf = state->sbox[index];
-        state->sbox[index] = (uint8_t)state->sbox[j];
-        state->sbox[j] = (uint8_t)buf;
-        j++;
-    }
+	u_char temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
+
 /*
- * RC4 Encrypt/Decrypt.
+ * Initialize an RC4 state buffer using the supplied key,
+ * which can have arbitrary length.
  */
-void rc4_crypt(rc4_state_t* state, const uint8_t *inbuf, uint8_t **outbuf, int buflen)
+void
+rc4_init(struct rc4_state *const state, const u_char *key, int keylen)
 {
-    int idx = 0;
-    uint8_t num, num2, num3;
-    if(*outbuf) {
-        free(*outbuf);
-    }
-    *outbuf = malloc(buflen);
-    if (*outbuf) {  // do not forget to test for failed allocation
-        while(idx != buflen) {
-            state->i = (int)(state->i + 1) % 0x100;
-            state->j = (int)(state->j + state->sbox[state->i]) % 0x100;
-            num = (uint8_t)state->sbox[state->i];
-            state->sbox[state->i] = (uint8_t)state->sbox[state->j];
-            state->sbox[state->j] = (uint8_t)num;
-            num2 = (uint8_t)inbuf[idx];
-            num3 = (uint8_t)state->sbox[(state->sbox[state->i] + (uint8_t)state->sbox[state->j]) % 0x100];
-            (*outbuf)[idx] = (uint8_t)(num2 ^ num3);
-            idx++;
-        }
-    }
+	u_char j;
+	int i;
+
+	/* Initialize state with identity permutation */
+	for (i = 0; i < 256; i++)
+		state->perm[i] = (u_char)i;
+	state->index1 = 0;
+	state->index2 = 0;
+
+	/* Randomize the permutation using key data */
+	for (j = i = 0; i < 256; i++) {
+		j += state->perm[i] + key[i % keylen];
+		swap_bytes(&state->perm[i], &state->perm[j]);
+	}
+}
+
+/*
+ * Encrypt some data using the supplied RC4 state buffer.
+ * The input and output buffers may be the same buffer.
+ * Since RC4 is a stream cypher, this function is used
+ * for both encryption and decryption.
+ */
+void
+rc4_crypt(struct rc4_state *const state,
+	const u_char *inbuf, u_char *outbuf, int buflen)
+{
+	int i;
+	u_char j;
+
+	for (i = 0; i < buflen; i++) {
+
+		/* Update modification indicies */
+		state->index1++;
+		state->index2 += state->perm[state->index1];
+
+		/* Modify permutation */
+		swap_bytes(&state->perm[state->index1],
+		    &state->perm[state->index2]);
+
+		/* Encrypt/decrypt next byte */
+		j = state->perm[state->index1] + state->perm[state->index2];
+		outbuf[i] = inbuf[i] ^ state->perm[j];
+	}
 }
